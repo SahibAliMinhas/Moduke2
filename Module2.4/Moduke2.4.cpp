4189e3b82b2806874974c8e8d5359228bfbf6549
sahib <minhasms@acad.sunybroome.edu>
2026-02-13 11:13:39 -0500

    updated to include 2.4

--- Module2.4/Moduke2.4.cpp
+++ Module2.4/Moduke2.4.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main()
 {
-	int num1, num2, num3, average;
-	num1 = 125;
-	num2 = 28;
-	num3 = -25;
+	double num1, num2, num3, average;
+	num1 = 75.35;
+	num2 = 35.56;
+	num3 = 15.76;
 	average = 0;
-	
+
 	average = (num1 + num2 + num3) / 3;
 	cout << "Num1: " << num1 << endl;
 	cout << "um2: " << num2 << endl;
